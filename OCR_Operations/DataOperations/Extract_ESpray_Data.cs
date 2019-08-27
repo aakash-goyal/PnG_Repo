acf6718aca535872fb14e56ed46d0b4e7a302b5a
aakash-goyal <aaakash.goyal1897@gmail.com>
2019-08-27 20:59:33 +0530

    espray filled

--- OCR_Operations/DataOperations/Extract_ESpray_Data.cs
+++ OCR_Operations/DataOperations/Extract_ESpray_Data.cs
@@ -17,112 +17,117 @@ namespace OCR_Operations.DataOperations
             string value;
             List<DataPointDefinition> dataPointDefinitions = GetDataPointDefinitions(cpeEntryId);
 
-            //OCRTEXT have 3 TS only 
-            foreach (var dataPointDefinition in dataPointDefinitions)
+            OCRText = OCRText.Replace("\r\nEmulsion Spray Header\r\n", "\r\n").Replace("\r\n= Fabric Angle\r\n", "\r\n").Replace("\r\n-1 to +1\r\n", "\r\n").Replace("\r\n1 to +1\r\n", "\r\n").Replace("\r\n|", "\r\n");
+            Regex to178Regex = new Regex(@"to\s178\s[0-9]");
+            if (to178Regex.IsMatch(OCRText))
+            {
+                OCRText = OCRText.Replace("to 178 ", "to 178\r\n");
+            }
+            Regex regex = new Regex(@"\r\n(([0-9]*(\.\d{1,3})?)|([0-9]*-[0-9]*\/[0-9]*))\s(([0-9]+(\.\d{1,3}))|([0-9]+-[0-9]*\/[0-9]*))\r\n");
+            while (regex.IsMatch(OCRText))
+            {
+                Match match = regex.Match(OCRText);
+                OCRText = OCRText.Replace(match.Value, match.Value.Replace(" ", "\r\n"));
+            }
+            Hashtable headValues = new Hashtable();
+
+            foreach (var dataPointDefinition in dataPointDefinitions.Where(item => item.IsCalculated == 0 && item.DPShortName != null))
             {
                 int labelIndex = GetLabelIndex("Emulsion Spray Header Angle");
                 if (dataPointDefinition.DataSetDefinitionId == 190)
                 {
+                    string tempOcrText = OCRText;
+                    OCRText = OCRText.Substring(0, GetLabelIndex("Emulsion Spray Header Distance"));
                     int slotNumber = 0;
-                    if (dataPointDefinition.Title.Contains("Fabric"))
-                    {
-                        if (dataPointDefinition.IsConstantValue == 1)
-                        {
-                            value = dataPointDefinition.ConstantValue;
-                        }
-                        else if (dataPointDefinition.Title.Contains("Target"))
-                        {
-                            slotNumber = 1;
-                            value_Label = dataPointDefinition.Title.Replace(" Target", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else if (dataPointDefinition.Title.Contains("TS"))
-                        {
-                            slotNumber = 2;
-                            value_Label = dataPointDefinition.Title.Replace(" TS", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else if (dataPointDefinition.Title.Contains("DS"))
-                        {
-                            slotNumber = 3;
-                            value_Label = dataPointDefinition.Title.Replace(" DS", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                        // To Handle error of Emulsion Spray Header Angle table
-                    }
-                    else if (dataPointDefinition.Title.Contains("Emulsion"))
-                    {
-                        labelIndex = GetFrontLabelIndex_SlotWise("Emulsion Spray Header", 3);
-                        if (dataPointDefinition.IsConstantValue == 1)
-                        {
-                            value = dataPointDefinition.ConstantValue;
-                        }
-                        else if (dataPointDefinition.Title.Contains("History"))
-                        {
-                            continue;
-                        }
-                        else if (dataPointDefinition.Title.Contains("Target"))
-                        {
-                            slotNumber = 1;
-                            value_Label = dataPointDefinition.Title.Replace(" Target", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else if (dataPointDefinition.Title.Contains("TS"))
-                        {
-                            slotNumber = 2;
-                            value_Label = dataPointDefinition.Title.Replace(" TS", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else if (dataPointDefinition.Title.Contains("DS"))
-                        {
-                            slotNumber = 3;
-                            value_Label = dataPointDefinition.Title.Replace(" DS", "");
-                            value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                        // To Handle error of Emulsion Spray Header Angle table
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-                else if (dataPointDefinition.DataSetDefinitionId == 191 && dataPointDefinition.Title.Contains("Distance nozzle tip to belt run"))
-                {
-                    int slotNumber = 0;
+                    value_Label = "Fabric Angle";
                     if (dataPointDefinition.IsConstantValue == 1)
                     {
                         value = dataPointDefinition.ConstantValue;
                     }
-                    else if (dataPointDefinition.Title.Contains("TS"))
+                    else if (dataPointDefinition.Title.Contains("Fabric Angle TS"))
+                    {
+                        slotNumber = 2;
+                        value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                        value = RemoveGeneralError_SteamHood(value);
+                    }
+                    else if (dataPointDefinition.Title.Contains("Fabric Angle DS"))
                     {
                         slotNumber = 3;
-                        value_Label = dataPointDefinition.Title.Replace(" TS", "");
                         value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                        value = RemoveGeneralError_SteamHood(value);
                     }
-                    else if (dataPointDefinition.Title.Contains("DS"))
+                    else if (dataPointDefinition.Title.Contains("Emulsion Spray Header TS"))
                     {
                         slotNumber = 4;
-                        value_Label = dataPointDefinition.Title.Replace(" DS", "");
                         value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                        value = RemoveGeneralError_SteamHood(value);
+                    }
+                    else if (dataPointDefinition.Title.Contains("Emulsion Spray Header DS"))
+                    {
+                        slotNumber = 5;
+                        value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                        value = RemoveGeneralError_SteamHood(value);
                     }
                     else
                     {
+                        OCRText = tempOcrText;
+                        continue;
+                    }
+                    OCRText = tempOcrText;
+                }
+                else if (dataPointDefinition.DataSetDefinitionId == 191 && dataPointDefinition.Title.Contains("Distance nozzle tip to belt run"))
+                {
+                    string tempOcrText = OCRText;
+                    OCRText = OCRText.Substring(0, GetLabelIndex("General Comments"));
+                    int slotNumber = 0;
+                    value_Label = "Distance nozzle tip to belt run";
+                    if (dataPointDefinition.IsConstantValue == 1)
+                    {
+                        value = dataPointDefinition.ConstantValue;
+                    }
+                    else if (dataPointDefinition.Title.EndsWith("TS"))
+                    {
+                        slotNumber = 3;
+                        value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                    }
+                    else if (dataPointDefinition.Title.EndsWith("DS"))
+                    {
+                        slotNumber = 4;
+                        value = GetSlotLabelValue_RefinerPlate(value_Label, slotNumber, labelIndex);
+                    }
+                    //Write Target field for variantId 180
+                    else
+                    {
+                        OCRText = tempOcrText;
                         continue;
                     }
                     // To Handle error of Emulsion Spray Header Distance table
+                    OCRText = tempOcrText;
                 }
-                // Add for DS table
                 else
                 {
                     continue;
+                }
+                if (value != ErrorText)
+                {
+                    headValues.Add(dataPointDefinition.Id, value);
+                }
+                else
+                {
+                    headValues.Add(dataPointDefinition.Id, 0);
+                }
+                // Add to Data List
+                cpeDataList.Add(CreateCpeEntryDataPoint(value, dataPointDefinition.Id, cpeDefinitionId, cpeEntryId));
+            }
+            foreach (var dataPointDefinition in dataPointDefinitions.Where(item => item.IsCalculated == 1))
+            {
+                int[] dataPoints = { 2784, 2785 };
+                double faTs = Convert.ToDouble(headValues[dataPoints[0]].ToString());
+                double faDs = Convert.ToDouble(headValues[dataPoints[1]].ToString());
+                value = Math.Round((faTs + faDs) / 2, 3).ToString();
+                if (faTs == 0 || faDs == 0)
+                {
+                    value = ErrorText;
                 }
                 // Add to Data List
                 cpeDataList.Add(CreateCpeEntryDataPoint(value, dataPointDefinition.Id, cpeDefinitionId, cpeEntryId));
